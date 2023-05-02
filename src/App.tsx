9bbfa9bdef1dfc35e2e6cafc7e0ed01b0d4efe57
onze <slapexs@gmail.com>
2023-05-02 21:30:34 +0700

    :sparklse: Create function toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 
 const App: FC = () => {
-	return <></>
+	const defaultTheme = localStorage.getItem("theme")
+
+	const setDarkmode = () => {
+		localStorage.setItem("theme", "dark")
+		document.querySelector("body")?.setAttribute("data-theme", "dark")
+	}
+	const setLightmode = () => {
+		localStorage.setItem("theme", "light")
+		document.querySelector("body")?.setAttribute("data-theme", "light")
+	}
+
+	const changeTheme = (e: { target: { checked: any } }) => {
+		e.target.checked ? setDarkmode() : setLightmode()
+	}
+
+	if (defaultTheme == "dark") {
+		setDarkmode()
+	}
+	return (
+		<>
+			<input
+				type="checkbox"
+				name="toggle-theme"
+				id="toggle-theme"
+				onChange={changeTheme}
+				defaultChecked={defaultTheme === "dark"}
+			/>
+		</>
+	)
 }
 
 export default App
