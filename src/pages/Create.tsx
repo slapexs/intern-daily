65363af9c4b3a96797009c363f8e903d0cb7573f
onze <slapexs@gmail.com>
2023-05-06 14:34:18 +0700

    :lipstick: Update form create new record

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,10 +1,30 @@
 import { FC } from "react"
 import Navbar from "../components/Navbar"
+import InputFields from "../components/InputField"
+import TextareaField from "../components/TextareaField"
+import FileInputField from "../components/fileInputField"
 
 const CratePage: FC = () => {
 	return (
-		<section>
-			Create Page <Navbar />
+		<section className="w-full flex justify-center">
+			<div className="w-11/12 mt-10">
+				<div className="mb-5 space-y-2">
+					<h1 className="font-bold text-3xl">Create new</h1>
+					<p className="font-light text-gray-500 tracking-wider">
+						เพิ่มบันทึกงานใหม่ ของวันนี้กันเลย!
+					</p>
+				</div>
+
+				<div className="my-10">
+					<form id="create-form" name="create-form">
+						<InputFields id="date-input" label="วันที่" type="date" />
+						<InputFields id="topic-input" label="หัวข้อ" type="text" />
+						<TextareaField id="detail-input" label="รายละเอียด" />
+						<FileInputField id="file-input" label="รูปภาพ" />
+					</form>
+				</div>
+			</div>
+			<Navbar />
 		</section>
 	)
 }
