5768297e54da03ead794dc7c88887f65c30a7564
onze <slapexs@gmail.com>
2023-05-08 14:55:07 +0700

    :package: Config libs for api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import express, { Express, Request, Response } from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app: Express = express()
 const port: number = 5000
+
+// Extension express
+app.use(express.json())
+app.use(cors())
 
 // Home page
 app.get("/", (req: Request, res: Response) =>
