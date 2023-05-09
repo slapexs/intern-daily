acefa35bc51a1ecc370f54f9880464169fabc2cb
onze <slapexs@gmail.com>
2023-05-09 22:43:55 +0700

    :sparkles: Create function authentication with jwt

--- src/model/User.ts
+++ src/model/User.ts
@@ -2,6 +2,8 @@ import { MongoClient, Db, Collection } from "mongodb"
 import bcrypt from "bcrypt"
 import dotenv from "dotenv"
 dotenv.config()
+import * as jwt from "jsonwebtoken"
+import { v4 as uuidv4 } from "uuid"
 
 const uri: any = process.env.DB_URI
 const client: MongoClient = new MongoClient(uri)
@@ -16,7 +18,7 @@ const findUsers = async () => {
 
 // Function check user already
 const checkUser = async (username: string) => {
-	const user = await collection.find({ username }).toArray()
+	const user = await collection.find({ username }).project({ _id: 0 }).toArray()
 	return user
 }
 
@@ -29,7 +31,9 @@ interface createUserProps {
 interface responseStatusProps {
 	status: string
 	statusCode: number
+	token?: any
 }
+
 const createUser = async ({ username, password, name }: createUserProps) => {
 	const user = await checkUser(username)
 	if (user.length > 0) {
@@ -41,8 +45,9 @@ const createUser = async ({ username, password, name }: createUserProps) => {
 	} else {
 		const salt = 10
 		const hashedPassword = bcrypt.hashSync(password, salt)
+		const userId: string = uuidv4()
 		// Create new user
-		collection.insertOne({ username, password: hashedPassword, name })
+		collection.insertOne({ userId, username, password: hashedPassword, name })
 		const response: responseStatusProps = {
 			status: "User created",
 			statusCode: 201,
@@ -51,4 +56,34 @@ const createUser = async ({ username, password, name }: createUserProps) => {
 	}
 }
 
-export { findUsers, createUser }
+// Authentication
+const authUser = async (username: string, password: string) => {
+	const findUser = await checkUser(username)
+	if (findUser.length < 1) {
+		const response: responseStatusProps = {
+			status: "Username was not found",
+			statusCode: 401,
+		}
+		return response
+	} else {
+		// check match username password
+		const passwordMatched = bcrypt.compareSync(password, findUser[0].password)
+		if (passwordMatched) {
+			const authSecret = "intern-daily"
+			const token = await jwt.sign(findUser[0].userId, authSecret)
+			const response: responseStatusProps = {
+				status: "Password is matched",
+				statusCode: 200,
+				token: token,
+			}
+			return response
+		} else {
+			const response: responseStatusProps = {
+				status: "Password is not match",
+				statusCode: 401,
+			}
+			return response
+		}
+	}
+}
+export { findUsers, createUser, authUser }
--- src/routers/User.ts
+++ src/routers/User.ts
@@ -1,7 +1,7 @@
 import express, { Router, Request, Response } from "express"
 
 // Model user
-import { findUsers, createUser } from "../model/User"
+import { findUsers, createUser, authUser } from "../model/User"
 
 const router: Router = express.Router()
 
@@ -11,8 +11,15 @@ router.get("/all", async (req: Request, res: Response) => {
 })
 
 router.post("/create", async (req: Request, res: Response) => {
-	const resStatus = await createUser(req.body)
-	res.status(resStatus.statusCode).json({ status: resStatus.status })
+	const { status, statusCode } = await createUser(req.body)
+	res.status(statusCode).json({ status })
+})
+
+// Authentication
+router.post("/login", async (req: Request, res: Response) => {
+	const { username, password } = req.body
+	const { status, statusCode, token } = await authUser(username, password)
+	res.status(statusCode).json({ status, token })
 })
 
 module.exports = router
