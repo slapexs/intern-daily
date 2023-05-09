481375089ab4afd8b997897bf93dd22872a38719
onze <slapexs@gmail.com>
2023-05-09 19:40:00 +0700

    :sparkles: Create function create new user

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,16 +1,54 @@
-import { MongoClient, Db } from "mongodb"
+import { MongoClient, Db, Collection } from "mongodb"
+import bcrypt from "bcrypt"
 import dotenv from "dotenv"
 dotenv.config()
 
 const uri: any = process.env.DB_URI
-const client = new MongoClient(uri)
+const client: MongoClient = new MongoClient(uri)
+const db: Db = client.db()
+const collection: Collection = db.collection("users")
 
-const db = client.db()
-
+// Function get all user
 const findUsers = async () => {
-	const collection = db.collection("users")
 	const users = collection.find().toArray()
 	return users
 }
 
-export { findUsers }
+// Function check user already
+const checkUser = async (username: string) => {
+	const user = await collection.find({ username }).toArray()
+	return user
+}
+
+interface createUserProps {
+	username: string
+	password: string
+	name: string
+}
+
+interface responseStatusProps {
+	status: string
+	statusCode: number
+}
+const createUser = async ({ username, password, name }: createUserProps) => {
+	const user = await checkUser(username)
+	if (user.length > 0) {
+		const response: responseStatusProps = {
+			status: "Username already exists",
+			statusCode: 401,
+		}
+		return response
+	} else {
+		const salt = 10
+		const hashedPassword = bcrypt.hashSync(password, salt)
+		// Create new user
+		collection.insertOne({ username, password: hashedPassword, name })
+		const response: responseStatusProps = {
+			status: "User created",
+			statusCode: 201,
+		}
+		return response
+	}
+}
+
+export { findUsers, createUser }
--- src/routers/User.ts
+++ src/routers/User.ts
@@ -1,13 +1,18 @@
 import express, { Router, Request, Response } from "express"
 
 // Model user
-import { findUsers } from "../model/User"
+import { findUsers, createUser } from "../model/User"
 
 const router: Router = express.Router()
 
 router.get("/all", async (req: Request, res: Response) => {
 	const users = await findUsers()
 	res.status(200).json({ users: users })
+})
+
+router.post("/create", async (req: Request, res: Response) => {
+	const resStatus = await createUser(req.body)
+	res.status(resStatus.statusCode).json({ status: resStatus.status })
 })
 
 module.exports = router
