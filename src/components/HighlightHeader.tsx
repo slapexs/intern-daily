4bab75c91d7a1e23926b4e56b4999e9ff2f421dc
onze <slapexs@gmail.com>
2023-05-17 23:07:45 +0700

    :memo: Add function fet record from api

--- src/components/HighlightHeader.tsx
+++ src/components/HighlightHeader.tsx
@@ -1,17 +1,27 @@
 import { useState, useEffect, FC } from "react"
 import ListRecord from "./ListRecord"
 import Gallery from "./Gallery"
+import { getRecordsProp } from "../pages/Find"
+import axios from "axios"
 
-const demoRecord = [1, 2, 3]
 const imageGallery = [
 	"https://images.unsplash.com/photo-1536148935331-408321065b18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
 	"https://images.unsplash.com/photo-1531973576160-7125cd663d86?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
 	"https://images.unsplash.com/photo-1581092795360-fd1ca04f0952?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
 	"https://images.unsplash.com/photo-1552664730-d307ca884978?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
 ]
+
 const HighlightHeader: FC = () => {
 	const [passedDay, setPassedDay] = useState<number>(0)
 	const [leftDay, setLeftDay] = useState<number>(120)
+
+	const [getRecords, setGetRecords] = useState<getRecordsProp[]>([])
+
+	useEffect(() => {
+		axios.get("http://localhost:5000/record/all").then((res) => {
+			setGetRecords(res.data.records)
+		})
+	}, [])
 
 	return (
 		<main>
@@ -29,8 +39,13 @@ const HighlightHeader: FC = () => {
 
 			<div className="mt-5">
 				<h1>บันทึกล่าสุด</h1>
-				{demoRecord.map((elem, index) => (
-					<ListRecord title={elem} key={index} />
+				{getRecords.map((elem, index) => (
+					<ListRecord
+						title={elem.topic}
+						date={elem.date}
+						recordId={elem.recordId}
+						key={index}
+					/>
 				))}
 			</div>
 
