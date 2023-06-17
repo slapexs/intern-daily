43aa711ff61b386dbc0ceff4d65ff8425c02302b
onze <slapexs@gmail.com>
2023-06-17 23:16:19 +0700

    :sparkles::lipstick: Create recent list timestamp

--- src/pages/Timestamp.tsx
+++ src/pages/Timestamp.tsx
@@ -3,16 +3,39 @@ import Navbar from "../components/Navbar"
 import InputFields from "../components/InputField"
 import axios from "axios"
 
-interface formatDate {
-	day: number
-	month: number
-	fullYear: number
-}
+type recentTimestamp = {
+	recordId: string
+	date: string
+	enterTime: string
+	leaveTime: string
+}[]
 
 const Timestamp: FC = () => {
 	const [dateTimestamp, setDateTimestamp] = useState<string>("")
 	const [enterTimestamp, setEnterTimestamp] = useState<string>("")
 	const [leaveTimestamp, setLeaveTimestamp] = useState<string>("")
+	const [listTimestamp, setListTimestamp] = useState<recentTimestamp>([])
+	const [limitRecentTimestamp, setLimitRecentTimestamp] = useState<string>("10")
+
+	// Function get recent timestamp
+	const getRecentTimestamp = async (e: any = null) => {
+		const authToken = localStorage.getItem("auth-token")
+		const response = await axios.post(
+			"http://localhost:5000/auth/user",
+			{},
+			{
+				headers: {
+					Authorization: `Bearer ${authToken}`,
+				},
+			}
+		)
+		const { data } = await axios.get(
+			`http://localhost:5000/timestamp/recent/${response.data.response.id}/${
+				!e ? limitRecentTimestamp : e.target.value
+			}`
+		)
+		setListTimestamp(data.data)
+	}
 
 	useEffect(() => {
 		const currentDate = new Date()
@@ -23,6 +46,9 @@ const Timestamp: FC = () => {
 
 		const formattedDate = `${year}-${month}-${day}`
 		setDateTimestamp(formattedDate)
+
+		// Get list timestamp
+		getRecentTimestamp()
 	}, [])
 
 	const submitTimestamp = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -49,12 +75,11 @@ const Timestamp: FC = () => {
 			alert(res.data.status)
 			setEnterTimestamp("")
 			setLeaveTimestamp("")
-		} catch (error) {
+		} catch (error: any) {
 			alert(error.response.data.status)
 			setEnterTimestamp("")
 			setLeaveTimestamp("")
 		}
-
 	}
 
 	return (
@@ -66,7 +91,6 @@ const Timestamp: FC = () => {
 						ลงเวลาทำงาน!
 					</p>
 				</div>
-
 				<form onSubmit={submitTimestamp}>
 					<div>
 						<InputFields
@@ -101,6 +125,50 @@ const Timestamp: FC = () => {
 						</button>
 					</div>
 				</form>
+				{/* List timestamps */}
+				<div className="my-10 pb-10">
+					<div className="flex justify-between items-center">
+						<div>
+							<p className=" text-gray-400">บันทึกเวลา</p>
+						</div>
+						<div>
+							<select
+								name="recentLimit"
+								id="recentLimit"
+								onChange={(e) => getRecentTimestamp(e)}
+							>
+								<option value="10" disabled>
+									จำนวนวัน
+								</option>
+								<option value="1">1</option>
+								<option value="7">7</option>
+								<option value="15">15</option>
+								<option value="30">30</option>
+							</select>
+						</div>
+					</div>
+					<ul>
+						{listTimestamp.map((elem, index) => (
+							<li
+								key={index}
+								className="bg-white rounded outline outline-1 outline-gray-200 my-3 p-3"
+							>
+								<section className="flex justify-between items-center">
+									<div>
+										<h1 className="font-semibold text-lg text-violet-500">
+											{elem.date}
+										</h1>
+									</div>
+									<div className="flex justify-around text-sm text-gray-400">
+										{elem.enterTime}
+										{" - "}
+										{elem.leaveTime}
+									</div>
+								</section>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 			<Navbar />
 		</section>
