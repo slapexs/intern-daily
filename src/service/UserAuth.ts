67a363629775a417108bf0b0f753d158e72f451c
onze <slapexs@gmail.com>
2023-06-24 16:34:35 +0700

    :sparkles: Create function get token from headers

--- src/service/UserAuth.ts
+++ src/service/UserAuth.ts
@@ -1,10 +1,23 @@
 import * as jwt from "jsonwebtoken"
 import { config } from "../../config/index"
 const JWTSecret: string = config.JWTSecret as string
+import { Request } from "express"
+export interface verifyUserProps {
+	id: string
+	username: string
+	iat: number
+}
 
-const verifyAuth = (authToken: any) => {
+const verifyAuth = (authToken: any): any => {
 	const verify = jwt.verify(authToken, JWTSecret)
 	return verify
 }
 
-export { verifyAuth }
+const getUserToken = (req: Request) => {
+	const authToken = req.headers.authorization
+	const token = authToken?.split(" ")[1] as string
+	const hasToken = authToken ? true : false
+	return { token, hasToken }
+}
+
+export { verifyAuth, getUserToken }
