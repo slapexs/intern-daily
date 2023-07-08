2c1879763aa2e020160a3f5d60fc1cf5b2198ea7
onze <slapexs@gmail.com>
2023-07-08 16:24:04 +0700

    :sparkles: Create function search record by date

--- src/pages/Find.tsx
+++ src/pages/Find.tsx
@@ -4,7 +4,7 @@ import InputFields from "../components/InputField"
 import ListRecord from "../components/ListRecord"
 import axios from "axios"
 
-import ArrowUpDownLineIcon from "remixicon-react/ArrowUpDownLineIcon"
+import CloseLineIcon from "remixicon-react/CloseLineIcon"
 
 export type getRecordsProp = {
 	recordId: string
@@ -17,8 +17,11 @@ export type getRecordsProp = {
 const FindPage: FC = () => {
 	const [getRecords, setGetRecords] = useState<getRecordsProp[]>([])
 	const [searchDate, setSearchDate] = useState<string>("")
+	const [canClear, setCanClear] = useState<boolean>(false)
 	const authToken = localStorage.getItem("auth-token")
-	useEffect(() => {
+
+	const GetMyRecord = async () => {
+		setCanClear(false)
 		axios
 			.post(
 				"http://localhost:5000/record/all",
@@ -28,7 +31,28 @@ const FindPage: FC = () => {
 			.then((res) => {
 				setGetRecords(res.data.records)
 			})
+	}
+	useEffect(() => {
+		GetMyRecord()
 	}, [])
+
+	// Search record by date
+	const SearchRecordByDate = async () => {
+		await axios
+			.post(
+				"http://localhost:5000/record/searchbydate",
+				{ date: searchDate },
+				{ headers: { Authorization: `Bearer ${authToken}` } }
+			)
+			.then((res) => {
+				if (res.status == 200) {
+					setGetRecords(res.data.data)
+					setCanClear(true)
+				} else {
+					alert(res.statusText)
+				}
+			})
+	}
 	return (
 		<section className="w-full flex justify-center mb-20">
 			<div className="w-10/12 mt-10">
@@ -50,7 +74,7 @@ const FindPage: FC = () => {
 					<button
 						type="button"
 						className="px-3 py-2 rounded bg-violet-500 text-white w-full"
-						onClick={() => console.log(searchDate)}
+						onClick={SearchRecordByDate}
 					>
 						ค้นหา
 					</button>
@@ -63,7 +87,12 @@ const FindPage: FC = () => {
 						</div>
 
 						<div>
-							<ArrowUpDownLineIcon />
+							{canClear && (
+								<CloseLineIcon
+									className="bg-violet-100 items-center rounded-lg flex text-sm"
+									onClick={GetMyRecord}
+								/>
+							)}
 						</div>
 					</div>
 
